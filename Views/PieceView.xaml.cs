3e3eb2aedf39c7ab897501836085ceefe40b5dcd
Paul ten Brink <info@psycro.com>
2026-01-16 11:49:58 +0100

    Drag and drop
    Introducing drag and drop

--- Views/PieceView.xaml.cs
+++ Views/PieceView.xaml.cs
@@ -34,12 +34,12 @@ namespace Check.Views
         {
             InitializeComponent();
 
-            Ellipse ellipse = new Ellipse { HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Stretch, Stroke = new SolidColorBrush(Colors.Black), StrokeThickness = 0.5d };
+            Ellipse = new Ellipse { HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Stretch, Stroke = new SolidColorBrush(Colors.Black), StrokeThickness = 0.5d };
 
-            ellipse.SetBinding(Shape.  FillProperty, new Binding(nameof(  Fill)) { Source = this } );
-            ellipse.SetBinding(Shape.StrokeProperty, new Binding(nameof(Stroke)) { Source = this } );
+            Ellipse.SetBinding(Shape.  FillProperty, new Binding(nameof(  Fill)) { Source = this } );
+            Ellipse.SetBinding(Shape.StrokeProperty, new Binding(nameof(Stroke)) { Source = this } );
 
-            Content = ellipse;
+            Content = Ellipse;
 
             MouseDown += PieceView_MouseDown;
 
@@ -53,6 +53,146 @@ namespace Check.Views
 
         public static readonly DependencyProperty   FillProperty = DependencyProperty.Register(nameof(Fill  ), typeof(Brush), typeof(PieceView));
         public static readonly DependencyProperty StrokeProperty = DependencyProperty.Register(nameof(Stroke), typeof(Brush), typeof(PieceView));
+
+        #endregion
+
+        #region Override methods
+
+        protected override void OnMouseMove(MouseEventArgs ea)
+        {
+            base.OnMouseMove(ea);
+
+            if (ea.LeftButton == MouseButtonState.Pressed)
+            {
+                // Package the data.
+                DataObject data = new DataObject();
+
+                data.SetData(DataFormats.StringFormat, Ellipse.Fill.ToString());
+                data.SetData("Double", Ellipse.Height);
+                data.SetData("Object", this);
+
+                // Initiate the drag-and-drop operation.
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
+            }
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs ea)
+        {
+            base.OnGiveFeedback(ea);
+
+            // These Effects values are set in the drop target's
+            // DragOver event handler.
+            if (ea.Effects.HasFlag(DragDropEffects.Copy))
+            {
+                Mouse.SetCursor(Cursors.Cross);
+            }
+            else if (ea.Effects.HasFlag(DragDropEffects.Move))
+            {
+                Mouse.SetCursor(Cursors.Pen);
+            }
+            else
+            {
+                Mouse.SetCursor(Cursors.No);
+            }
+
+            ea.Handled = true;
+        }
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush,
+                // convert it and apply it to the ellipse.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    Brush newFill = (Brush)converter.ConvertFromString(dataString);
+                    Ellipse.Fill = newFill;
+
+                    // Set Effects to notify the drag source what effect
+                    // the drag-and-drop operation had.
+                    // (Copy if CTRL is pressed; otherwise, move.)
+                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                    {
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            e.Effects = DragDropEffects.None;
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, allow copying or moving.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    // Set Effects to notify the drag source what effect
+                    // the drag-and-drop operation will have. These values are
+                    // used by the drag source's GiveFeedback event handler.
+                    // (Copy if CTRL is pressed; otherwise, move.)
+                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                    {
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        private Brush _previousFill;
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            base.OnDragEnter(e);
+
+            // Save the current Fill brush so that you can revert back to this value in DragLeave.
+            _previousFill = Ellipse.Fill;
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, convert it.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    Brush newFill = (Brush)converter.ConvertFromString(dataString.ToString());
+                    Ellipse.Fill = newFill;
+                }
+            }
+        }
+
+        protected override void OnDragLeave(DragEventArgs e)
+        {
+            base.OnDragLeave(e);
+            // Undo the preview that was applied in OnDragEnter.
+            Ellipse.Fill = _previousFill;
+        }
 
         #endregion
 
@@ -110,6 +250,12 @@ namespace Check.Views
                 }
             }
         }
+
+        #endregion
+
+        #region Private properties
+
+        private Ellipse Ellipse { get; set; }
 
         #endregion
     }
