5cd8111a34b701f1409370619c4dbda05973ab54
Paul ten Brink <info@psycro.com>
2026-05-17 16:42:44 +0200

    User interface
    Work on editing position

--- Views/SettingsView.xaml.cs
+++ Views/SettingsView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using Check.Models;
+using Check.ViewModels;
 
 namespace Check.Views
 {
@@ -16,49 +18,60 @@ namespace Check.Views
 
             FontSize     = 18d;
 
-            Grid grid = new Grid { Margin = new Thickness(0d, 0d, 24d, 0d ) } ;
+            StackPanel stackPanelMain      = new StackPanel { Orientation = Orientation.Vertical        } ;
+            StackPanel stackPanelSelection = new StackPanel { Orientation = Orientation.Horizontal      } ;
+            StackPanel stackPanelEditing   = new StackPanel { Orientation = Orientation.Vertical        } ;
 
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-                grid.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            Grid       gridPlaying         = new Grid       { Margin = new Thickness(0d, 24d, 24d, 0d ) } ;
 
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width  = new GridLength(200d) } );
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width  = new GridLength(200d) } );
+            stackPanelMain.Children.Add(stackPanelSelection);
+            stackPanelMain.Children.Add(gridPlaying        );
+            stackPanelMain.Children.Add(stackPanelEditing  );
 
-            Grid editGrid = new Grid();
+            Content = stackPanelMain;
 
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
-            editGrid.RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            RadioButton radioButtonPlaying = new RadioButton { Content = new TextBlock { Text = "Playing", FontSize = 24 } , VerticalContentAlignment = VerticalAlignment.Center , Margin = new Thickness( 4d, 0d, 52d, 0d) } ; radioButtonPlaying.Checked += delegate { gridPlaying.Visibility = Visibility.Visible; stackPanelEditing.Visibility = Visibility.Hidden ; } ;
+            RadioButton radioButtonEditing = new RadioButton { Content = new TextBlock { Text = "Editing", FontSize = 24 } , VerticalContentAlignment = VerticalAlignment.Center , Margin = new Thickness(52d, 0d,  0d, 0d) } ; radioButtonEditing.Checked += delegate { gridPlaying.Visibility = Visibility.Hidden ; stackPanelEditing.Visibility = Visibility.Visible; } ;
 
-            TextBlock textBlock = new TextBlock {                           Text = "Edit board",                 FontWeight = FontWeights.Bold,                              Margin = new Thickness(4d)                                                        } ;                                          Grid.SetRow(textBlock, 0); Grid.SetColumn(textBlock, 0); editGrid.Children.Add(textBlock);
+            radioButtonPlaying.IsChecked   = true;
 
-                      textBlock = new TextBlock {                           Text = "Settings"  , FontSize = 24d, FontWeight = FontWeights.Bold,                              Margin = new Thickness(4d)                                                        } ;                                          Grid.SetRow(textBlock, 0); Grid.SetColumn(textBlock, 0); grid.Children.Add(textBlock);
-                                                                                                                                                                                                                                                                                                            Grid.SetRow(editGrid , 1); Grid.SetColumn(editGrid , 0); grid.Children.Add(editGrid ); Grid.SetColumnSpan(editGrid, 2);
-            Button    button    = new Button    { Content = new TextBlock { Text = "Undo move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Undo last move <Ctrl+Z>"                     } ; button  .Click += OnUndoMove             ; Grid.SetRow(button   , 2); Grid.SetColumn(button   , 0); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Redo move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Redo last move <Ctrl+Y>"                     } ; button  .Click += OnRedoMove             ; Grid.SetRow(button   , 2); Grid.SetColumn(button   , 1); grid.Children.Add(button   );
-            CheckBox  checkBox  = new CheckBox  { Content = new TextBlock { Text = "Automatic moves"       , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Automatically perform forced moves <Ctrl+A>" } ; checkBox.Click += OnAutomaticMoves       ; Grid.SetRow(checkBox , 3); Grid.SetColumn(checkBox , 0); grid.Children.Add(checkBox );
-                      checkBox  = new CheckBox  { Content = new TextBlock { Text = "Feedback for moves"    , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Feedback for possible moves <Ctrl+F>"        } ; checkBox.Click += OnFeedbackForMoves     ; Grid.SetRow(checkBox , 3); Grid.SetColumn(checkBox , 1); grid.Children.Add(checkBox );
-                      checkBox  = new CheckBox  { Content = new TextBlock { Text = "Intermediate positions", VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Show intermediate positions <Ctrl+I>"        } ; checkBox.Click += OnIntermediatePositions; Grid.SetRow(checkBox , 4); Grid.SetColumn(checkBox , 0); grid.Children.Add(checkBox );
-                      button    = new Button    { Content = new TextBlock { Text = "Load stored position"  , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Load stored position <Ctrl+L>"               } ; button  .Click += OnLoadPosition         ; Grid.SetRow(button   , 5); Grid.SetColumn(button   , 0); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Save current position" , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Save current position <Ctrl+S>"              } ; button  .Click += OnSavePosition         ; Grid.SetRow(button   , 5); Grid.SetColumn(button   , 1); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Do a move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Do a move <Ctrl+M>"                          } ; button  .Click += OnMove                 ; Grid.SetRow(button   , 6); Grid.SetColumn(button   , 0); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Play until end"        , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Play until end <Ctrl+P>"                     } ; button  .Click += OnPlay                 ; Grid.SetRow(button   , 6); Grid.SetColumn(button   , 1); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Flip the board"        , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Flip the board"                              } ; button  .Click += OnFlipBoard            ; Grid.SetRow(button   , 7); Grid.SetColumn(button   , 0); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Flip whose turn it is" , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Flip whose turn it is"                       } ; button  .Click += OnFlipTurn             ; Grid.SetRow(button   , 7); Grid.SetColumn(button   , 1); grid.Children.Add(button   );
-                      button    = new Button    { Content = new TextBlock { Text = "Solve combination"     , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Solve combination"                           } ; button  .Click += OnSolveCombination     ; Grid.SetRow(button   , 8); Grid.SetColumn(button   , 0); grid.Children.Add(button   );
+            stackPanelSelection.Children.Add(radioButtonPlaying);
+            stackPanelSelection.Children.Add(radioButtonEditing);
 
-            Content = grid;
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+            gridPlaying.   RowDefinitions.Add(new    RowDefinition { Height = new GridLength( 40D) } );
+
+            gridPlaying.ColumnDefinitions.Add(new ColumnDefinition { Width  = new GridLength(200d) } );
+            gridPlaying.ColumnDefinitions.Add(new ColumnDefinition { Width  = new GridLength(200d) } );
+
+            Button    buttonUndoMove         = new Button    { Content = new TextBlock { Text = "Undo move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Undo last move <Ctrl+Z>"                     } ; buttonUndoMove         .Click += OnUndoMove             ; Grid.SetRow(buttonUndoMove         , 0); Grid.SetColumn(buttonUndoMove         , 0); gridPlaying.Children.Add(buttonUndoMove        );
+            Button    buttonRedoMove         = new Button    { Content = new TextBlock { Text = "Redo move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Redo last move <Ctrl+Y>"                     } ; buttonRedoMove         .Click += OnRedoMove             ; Grid.SetRow(buttonRedoMove         , 0); Grid.SetColumn(buttonRedoMove         , 1); gridPlaying.Children.Add(buttonRedoMove        );
+            CheckBox  checkBoxAutomaticMoves = new CheckBox  { Content = new TextBlock { Text = "Automatic moves"       , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Automatically perform forced moves <Ctrl+A>" } ; checkBoxAutomaticMoves .Click += OnAutomaticMoves       ; Grid.SetRow(checkBoxAutomaticMoves , 1); Grid.SetColumn(checkBoxAutomaticMoves , 0); gridPlaying.Children.Add(checkBoxAutomaticMoves);
+            CheckBox  checkBoxFeedback       = new CheckBox  { Content = new TextBlock { Text = "Feedback for moves"    , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Feedback for possible moves <Ctrl+F>"        } ; checkBoxFeedback       .Click += OnFeedbackForMoves     ; Grid.SetRow(checkBoxFeedback       , 1); Grid.SetColumn(checkBoxFeedback       , 1); gridPlaying.Children.Add(checkBoxFeedback      );
+            CheckBox  checkBoxIntermediate   = new CheckBox  { Content = new TextBlock { Text = "Intermediate positions", VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Show intermediate positions <Ctrl+I>"        } ; checkBoxIntermediate   .Click += OnIntermediatePositions; Grid.SetRow(checkBoxIntermediate   , 2); Grid.SetColumn(checkBoxIntermediate   , 0); gridPlaying.Children.Add(checkBoxIntermediate  );
+            Button    buttonLoadPosition     = new Button    { Content = new TextBlock { Text = "Load stored position"  , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Load stored position <Ctrl+L>"               } ; buttonLoadPosition     .Click += OnLoadPosition         ; Grid.SetRow(buttonLoadPosition     , 3); Grid.SetColumn(buttonLoadPosition     , 0); gridPlaying.Children.Add(buttonLoadPosition    );
+            Button    buttonSavePosition     = new Button    { Content = new TextBlock { Text = "Save current position" , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Save current position <Ctrl+S>"              } ; buttonSavePosition     .Click += OnSavePosition         ; Grid.SetRow(buttonSavePosition     , 3); Grid.SetColumn(buttonSavePosition     , 1); gridPlaying.Children.Add(buttonSavePosition    );
+            Button    buttonMove             = new Button    { Content = new TextBlock { Text = "Do a move"             , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Do a move <Ctrl+M>"                          } ; buttonMove             .Click += OnMove                 ; Grid.SetRow(buttonMove             , 4); Grid.SetColumn(buttonMove             , 0); gridPlaying.Children.Add(buttonMove            );
+            Button    buttonPlay             = new Button    { Content = new TextBlock { Text = "Play until end"        , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Play until end <Ctrl+P>"                     } ; buttonPlay             .Click += OnPlay                 ; Grid.SetRow(buttonPlay             , 4); Grid.SetColumn(buttonPlay             , 1); gridPlaying.Children.Add(buttonPlay            );
+            Button    buttonFlipBoard        = new Button    { Content = new TextBlock { Text = "Flip the board"        , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Flip the board"                              } ; buttonFlipBoard        .Click += OnFlipBoard            ; Grid.SetRow(buttonFlipBoard        , 5); Grid.SetColumn(buttonFlipBoard        , 0); gridPlaying.Children.Add(buttonFlipBoard       );
+            Button    buttonFlipTurn         = new Button    { Content = new TextBlock { Text = "Flip whose turn it is" , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Flip whose turn it is"                       } ; buttonFlipTurn         .Click += OnFlipTurn             ; Grid.SetRow(buttonFlipTurn         , 5); Grid.SetColumn(buttonFlipTurn         , 1); gridPlaying.Children.Add(buttonFlipTurn        );
+            Button    buttonSolve            = new Button    { Content = new TextBlock { Text = "Solve combination"     , VerticalAlignment = VerticalAlignment.Center }, Height = 28d, Margin = new Thickness(4d), ToolTip="Solve combination"                           } ; buttonSolve            .Click += OnSolveCombination     ; Grid.SetRow(buttonSolve            , 6); Grid.SetColumn(buttonSolve            , 0); gridPlaying.Children.Add(buttonSolve           );
+
+            Position                   positionDummy = new Position         (                         );
+            PositionViewModel positionViewModelDummy = new PositionViewModel(positionDummy            );
+            PositionView           positionViewDummy = new PositionView(positionViewModelDummy        );
+            FieldViewModel       fieldViewModelDummy = new    FieldViewModel(positionViewModelDummy, 1);
+
+            FieldView             fieldViewEmpty     = new FieldView(positionViewDummy, fieldViewModelDummy, 1); stackPanelEditing.Children.Add(fieldViewEmpty    );
+            FieldView             fieldViewWhiteMan  = new FieldView(positionViewDummy, fieldViewModelDummy, 1); stackPanelEditing.Children.Add(fieldViewWhiteMan );
+            FieldView             fieldViewBlackMan  = new FieldView(positionViewDummy, fieldViewModelDummy, 1); stackPanelEditing.Children.Add(fieldViewBlackMan );
+            FieldView             fieldViewWhiteKing = new FieldView(positionViewDummy, fieldViewModelDummy, 1); stackPanelEditing.Children.Add(fieldViewWhiteKing);
+            FieldView             fieldViewBlackKing = new FieldView(positionViewDummy, fieldViewModelDummy, 1); stackPanelEditing.Children.Add(fieldViewBlackKing);
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             void OnUndoMove             (object sender, RoutedEventArgs e) { PositionView.UndoLastMove                      (); }
