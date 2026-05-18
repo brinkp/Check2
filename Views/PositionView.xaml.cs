018a4b78ff2bd8441058a0e82f38aa2031d790ea
Paul ten Brink <info@psycro.com>
2026-05-18 15:53:36 +0200

    User interface
    Work on editing position

--- Views/PositionView.xaml.cs
+++ Views/PositionView.xaml.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
